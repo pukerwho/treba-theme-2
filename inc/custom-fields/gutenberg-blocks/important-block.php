1cc647831a222a94d62de50a410a99b4672208cf
Puker Who <nawwesele@gmail.com>
2023-11-16 11:49:10 +0200

    edit

--- inc/custom-fields/gutenberg-blocks/important-block.php
+++ inc/custom-fields/gutenberg-blocks/important-block.php
@@ -10,7 +10,7 @@ Block::make( 'TREBA Important Block' )
 	->set_render_callback( function ( $fields, $attributes, $inner_blocks ) {
 		?>
     
-    <div class="bg-emerald-100 border-l-8 border-emerald-500 rounded px-4 pl-4 lg:pl-8 py-4 mb-4">
+    <div class="bg-slate-300 dark:bg-slate-800 border-l-8 border-slate-700 dark:border-slate-400 rounded px-4 pl-4 lg:pl-8 py-4 mb-4">
       <div class="content">
         <p><?php echo esc_html( $fields['crb_post_important_block'] ); ?></p>
       </div>
